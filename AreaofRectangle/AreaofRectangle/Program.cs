f86b95d3daedec1ebc61b346f1ee1bd4ccb38da8
nerdelt <nerdelt@gmail.com>
2018-07-18 11:39:55 -0500

    Assignment 2, Area of Rectangle

---
+++ AreaofRectangle/AreaofRectangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AreaofRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h;
+            int y;
+
+            Console.WriteLine("Lets find the area of your rectangle!");
+            Console.Write("Please type the height of the rectangle: ");
+            h = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please type the width of the rectangle: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            int area = h*y;
+            Console.WriteLine("The area of the rectangle is: {0}", area);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
