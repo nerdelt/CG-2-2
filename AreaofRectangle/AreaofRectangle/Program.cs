b87816e6c8e42f516da4b453b1d39a3e4b668f67
nerdelt <41270734+nerdelt@users.noreply.github.com>
2018-07-18 19:35:06 -0500

    Update Program.cs

--- AreaofRectangle/AreaofRectangle/Program.cs
+++ AreaofRectangle/AreaofRectangle/Program.cs
@@ -6,15 +6,15 @@ namespace AreaofRectangle
     {
         static void Main(string[] args)
         {
-            int h;
-            int y;
+            int h = int.Parse(h);
+            int y = int.Parse(y);
 
             Console.WriteLine("Let's find the area of your rectangle!");
             Console.Write("Please type the height of the rectangle: ");
-            int h = Console.ReadLine();
+            string h = Console.ReadLine();
 
             Console.Write("Please type the width of the rectangle: ");
-            int y =Console.ReadLine();
+            string y =Console.ReadLine();
 
             int area = h*y;
             Console.WriteLine("The area of the rectangle is: {0}", area);
