07f465e214f09ac4cb9e9687cf053311e98fb588
nerdelt <41270734+nerdelt@users.noreply.github.com>
2018-07-19 10:16:38 -0500

    Update Program.cs

--- AreaofRectangle/AreaofRectangle/Program.cs
+++ AreaofRectangle/AreaofRectangle/Program.cs
@@ -9,10 +9,10 @@ namespace AreaofRectangle
 
             Console.WriteLine("Let's find the area of your rectangle!");
             Console.Write("Please type the height of the rectangle: ");
-            decimal.lengthSide = decimal.Parse(Console.ReadLine()); 
+            decimal lengthSide = decimal.Parse(Console.ReadLine()); 
 
             Console.Write("Please type the width of the rectangle: ");
-            decimal.widthSide = decimal.Parse(Console.ReadLine());
+            decimal widthSide = decimal.Parse(Console.ReadLine());
 
             decimal area = lengthside * widthSide;
             Console.WriteLine("The area of the rectangle is: {0}", area);
