ec8d8998da65cfe21cb6537f4b1ff1e44e630365
nerdelt <nerdelt@gmail.com>
2018-07-19 22:50:36 -0500

    fixed my mistake.

--- AreaofRectangle/AreaofRectangle/Program.cs
+++ AreaofRectangle/AreaofRectangle/Program.cs
@@ -5,18 +5,19 @@ namespace AreaofRectangle
     class Program
     {
         static void Main(string[] args)
+        
         {
-            int h = int.Parse(h);
-            int y = int.Parse(y);
+   
 
             Console.WriteLine("Let's find the area of your rectangle!");
             Console.Write("Please type the height of the rectangle: ");
-            string h = Console.ReadLine();
+            decimal lengthSide = decimal.Parse(Console.ReadLine());
 
             Console.Write("Please type the width of the rectangle: ");
-            string y =Console.ReadLine();
-
-            int area = h*y;
+            decimal widthSide = decimal.Parse(Console.ReadLine());
+             
+            //This is where I messed up and realized I need to pay more attention to detail.
+            decimal area = lengthSide * widthSide;
             Console.WriteLine("The area of the rectangle is: {0}", area);
             Console.ReadLine();
 
